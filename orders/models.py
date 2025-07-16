677c8120bc1aa653233a7158b5d10a87dc701cb9
Erasyl <erasylam@yandex.kz>
2025-07-16 15:34:43 +0500

    added models in orders

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from shop.models import Product
 from django.conf import settings
-
+from  decimal import  Decimal
+from  django.core.validators import  MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -14,7 +16,9 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
-    
+    coupon = models.ForeignKey(Coupon, related_name='orders', on_delete=models.SET_NULL, blank=True, null=True)
+    discount = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(100)])
+
     
     class Meta:
         ordering = ['-created']
@@ -27,10 +31,10 @@ class Order(models.Model):
         return f'Order {self.id}'
     
     
-    def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
-    
-    
+    # def get_total_cost(self):
+    #     total_cost =  self.get_total_cost_before_discount()
+    #     return  total_cost - self.get_discount()
+    #
     def get_stripe_url(self):
         if not self.stripe_id:
             return ''
@@ -39,8 +43,19 @@ class Order(models.Model):
         else:
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
-    
-    
+
+    def get_total_cost_before_discount(self):
+        return  sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return  total_cost * (self.discount / Decimal(100))
+        return  Decimal(0)
+
+    def get_total_cost(self):
+        total_cost =  self.get_total_cost_before_discount()
+        return  total_cost - self.get_discount()
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
                               related_name='items',
